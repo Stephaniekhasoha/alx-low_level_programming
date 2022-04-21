97778b538f02dc680e785ec6bd62fa08599143db
Stephaniekhasoha <khasohastephanie@gmail.com>
2022-04-20 17:38:21 -0700

    c pointers arrays and string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array.
+ * @a: the array to print.
+ * @n: array's length
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src, including \0.
+ *@src: pointer.
+ *@dest: pointer.
+ *Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
