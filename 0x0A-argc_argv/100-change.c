b83a27eebb8958363a68aafcac9fa47d3d4471e0
Stephaniekhasoha <khasohastephanie@gmail.com>
2022-05-03 12:50:42 +0300

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints the minimum number of coins
- *        to make change for an amount of money.
- *
- * @argc: holds the number of arguments passed
- * @argv: array pointer that holds the arguments passed
- *
- * Return: Always 0 (Success)
-*/
+ * main - prints the minimum number of coins for an amount of money
+ * @argc: should count two arguments
+ * @argv: arguments given should be program name and amount of money
+ * Return: least number of coins, 0 if negative amount, 1 if amount not given
+ */
 
 int main(int argc, char *argv[])
 {
-	int cents, coins_number = 0;
+	int n, cents = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
+
+	if (argv[1][0] == '-')
 	{
-		cents = atoi(argv[1]);
-		while (cents > 0)
-		{
-			if (cents >= 25)
-				cents -= 25;
-			else if (cents >= 10)
-				cents -= 10;
-			else if (cents >= 5)
-				cents -= 5;
-			else if (cents >= 2)
-				cents -= 2;
-			else if (cents >= 1)
-				cents -= 1;
-			coins_number += 1;
-		}
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", coins_number);
+
+	n = atoi(argv[1]);
+
+	cents += n / 25;
+	n = n % 25;
+	cents += n / 10;
+	n = n % 10;
+	cents += n / 5;
+	n = n % 5;
+	cents += n / 2;
+	n = n % 2;
+	cents += n / 1;
+
+	printf("%d\n", cents);
 	return (0);
 }
